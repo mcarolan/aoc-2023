6788123fbedbcf55e19dd15650890555ba4f2dd2
Martin Carolan <martin@typesystems.co.uk>
2023-12-11 17:51:33 +1300

    Hacked together day 10. needs a tidy

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::{collections::{HashMap, HashSet}};
+
 
 advent_of_code::solution!(10);
 
@@ -38,7 +39,10 @@ struct Pipe {
 #[derive(Debug)]
 struct Input {
     tiles: HashMap<(i32, i32), Pipe>,
+    ground: HashSet<(i32, i32)>,
     start: (i32, i32),
+    width: i32,
+    height: i32
 }
 
 fn parse_pipe(input: char) -> Pipe {
@@ -77,11 +81,7 @@ fn valid_next(
     map: &HashMap<(i32, i32), Pipe>,
 ) -> Option<((i32, i32), Heading)> {
     let to = heading.offset(from);
-    let to_pipe_opt = map.get(&to);
-    if to_pipe_opt.is_none() {
-        return None;
-    }
-    let to_pipe = to_pipe_opt.unwrap();
+    let to_pipe = map.get(&to).unwrap();
 
     let new_heading = if to_pipe.from != heading && to_pipe.from != heading.opposite() {
         to_pipe.from
@@ -120,18 +120,47 @@ fn furthest(start: (i32, i32), map: &HashMap<(i32, i32), Pipe>) -> Option<i32> {
     }
 }
 
+fn loop_path(start: (i32, i32), map: &HashMap<(i32, i32), Pipe>) -> HashMap<(i32, i32), Pipe> {
+    let mut at = start;
+    let mut heading = map.get(&start).unwrap().to;
+
+    let mut result: HashMap<(i32, i32), Pipe> = HashMap::new();
+    result.insert(at, *map.get(&at).unwrap());
+
+    loop {
+        match valid_next(&at, heading, map) {
+            Some((to, new_heading)) => {
+                if to == start {
+                    break;
+                }
+                at = to;
+                result.insert(at, *map.get(&at).unwrap());
+                heading = new_heading;
+            }
+            None => break,
+        }
+    }
+
+    result
+}
+
 fn parse_input(input: &str) -> Input {
     let mut start: Option<(i32, i32)> = None;
     let mut y = 0;
+    let mut x = 0;
 
+    let mut ground: HashSet<(i32, i32)> = HashSet::new();
     let mut map: HashMap<(i32, i32), Pipe> = HashMap::new();
 
     for line in input.lines() {
-        let mut x = 0;
+        x = 0;
         for char in line.chars() {
             match char {
                 'S' => start = Some((x, y)),
-                '.' => (),
+                '.' => {
+                    ground.insert((x, y));
+                    ()
+                },
                 _ => {
                     map.insert((x, y), parse_pipe(char));
                     ()
@@ -146,7 +175,10 @@ fn parse_input(input: &str) -> Input {
 
     Input {
         start: start.unwrap(),
-        tiles: map
+        tiles: map,
+        height: y,
+        width: x,
+        ground
     }
 }
 
@@ -188,13 +220,90 @@ pub fn part_one(input: &str) -> Option<i32> {
         map.insert(input.start, start_pipe);
         let result = furthest(input.start, &map);
         results.push(result);
+        println!("{:?} score for {:?}", result, start_pipe);
     }
 
     results.into_iter().flatten().max()
 }
 
+fn double_tuple(tup: &(i32, i32)) -> (i32, i32) {
+    (2*tup.0, 2*tup.1)
+}
+
+fn double_res(tiles: &HashMap<(i32, i32), Pipe>) -> HashSet<(i32, i32)> {
+    let res: HashSet<(i32, i32)> = tiles.iter().flat_map(|(pos, pipe)| {
+        vec! [
+            pipe.from.offset(&double_tuple(pos)),
+            double_tuple(pos),
+            pipe.to.offset(&double_tuple(pos)),
+        ]
+    }).collect();
+
+    res
+}
+
+
+fn clamp(pos: &(i32, i32), width: i32, height: i32) -> Option<(i32, i32)> {
+    if pos.0 > width || pos.1 > height || pos.0 < 0 || pos.1 < 0 {
+        None
+    }
+    else {
+        Some(*pos)
+    }
+}
+
 pub fn part_two(input: &str) -> Option<i32> {
-    None
+    let input = parse_input(input);
+
+    let mut map = input.tiles.clone();
+    map.insert(input.start, Pipe { from: Heading::South, to: Heading::East });
+
+    let longest_loop = loop_path(input.start, &map);
+    let path_points = double_res(&longest_loop);
+
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut queue : Vec<(i32, i32)> = vec![];
+
+    for x in 0..2*input.width {
+        queue.push((x, 0));
+        queue.push((x, (2*input.height) - 1));
+    }
+    for y in 0..2*input.height {
+        queue.push((0, y));
+        queue.push(((2*input.width) - 1, y));
+    }
+
+    while let Some(at) = queue.pop() {
+        if visited.contains(&at) || path_points.contains(&at) {
+            continue;
+        }
+        visited.insert(at);
+
+        let neighbours : Vec<(i32, i32)> = vec![
+            Heading::North.offset(&at),
+            Heading::South.offset(&at),
+            Heading::East.offset(&at),
+            Heading::West.offset(&at)
+        ].iter().flat_map(|p| clamp(p, 2*input.width, 2*input.height)).collect();
+
+        for neighbour in neighbours {
+            queue.push(neighbour);
+        }
+    }
+
+    let mut counter = 0;
+
+    let longest: HashSet<&(i32, i32)> = longest_loop.keys().collect();
+
+    for x in 0..input.width {
+        for y in 0..input.height {
+            if !longest.contains(&(x, y)) && !visited.contains(&(2*x, 2*y)) {
+                counter += 1;
+            }
+        }
+    }
+
+    Some(counter)
 }
 
 #[cfg(test)]
