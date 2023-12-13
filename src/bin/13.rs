7864eadc9ce05282a008150e59d12b4df787e006
Martin Carolan <martin@typesystems.co.uk>
2023-12-13 20:10:22 +1300

    tidy

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,7 +1,3 @@
-use core::fmt;
-use std::ops::Deref;
-
-use itertools::Itertools;
 use nom::{
     branch::alt,
     character::complete::{self, newline},
@@ -37,56 +33,13 @@ impl Pattern {
     }
 }
 
-impl std::fmt::Display for Input {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for p in self.patterns.iter() {
-            writeln!(f, "{}", p)?;
-        }
-        Ok(())
-    }
-}
-
-impl std::fmt::Display for Pattern {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for row in self.rows.iter() {
-            for col in row {
-                if *col {
-                    write!(f, "#")?;
-                } else {
-                    write!(f, ".")?;
-                }
-            }
-            writeln!(f, "")?;
-        }
-        Ok(())
-    }
-}
-
-fn is_row_reflection(pattern: &Pattern, point: u32) -> bool {
-    let mut l = point as i32 - 1;
-    let mut r = point;
-
-    while l >= 0 && r < pattern.rows.len() as u32 {
-        if pattern.rows.get(l as usize) != pattern.rows.get(r as usize) {
-            return false;
-        }
-        l -= 1;
-        r += 1;
-    }
-    true
-}
-
-fn is_row_reflection2(pattern: &Pattern, point: u32) -> bool {
+fn is_row_reflection(pattern: &Pattern, point: u32, expected_diff: usize) -> bool {
     let mut l = point as i32 - 1;
     let mut r = point;
 
     let mut diffs = 0;
 
-    while l >= 0 && r < pattern.rows.len() as u32 {
-        if diffs > 1 {
-            break
-        }
-
+    while l >= 0 && r < pattern.rows.len() as u32 && diffs <= expected_diff {
         let r1 = pattern.rows.get(l as usize).unwrap();
         let r2 = pattern.rows.get(r as usize).unwrap();
 
@@ -96,7 +49,7 @@ fn is_row_reflection2(pattern: &Pattern, point: u32) -> bool {
         r += 1;
     }
 
-    diffs == 1
+    diffs == expected_diff
 }
 
 fn parse_pattern(input: &str) -> IResult<&str, Pattern> {
@@ -120,27 +73,33 @@ fn parse_input(input: &str) -> IResult<&str, Input> {
 struct Input {
     patterns: Vec<Pattern>,
 }
-pub fn part_one(input: &str) -> Option<u32> {
+
+fn solve(input: &str, expected_diff: usize) -> Option<u32> {
     let (_, input) = parse_input(input).unwrap();
 
     let mut result: u32 = 0;
 
     for pattern in input.patterns.iter() {
+        let mut done = false;
+
         for p in 1..pattern.rows.len() {
             let p = p as u32;
-            if is_row_reflection(&pattern, p) {
+            if is_row_reflection(&pattern, p, expected_diff) {
                 result += p * 100;
+                done = true;
                 break;
             }
         }
 
-        let tp = pattern.transpose();
+        if !done {
+            let tp = pattern.transpose();
 
-        for p in 1..tp.rows.len() {
-            let p = p as u32;
-            if is_row_reflection(&tp, p) {
-                result += p;
-                break;
+            for p in 1..tp.rows.len() {
+                let p = p as u32;
+                if is_row_reflection(&tp, p, expected_diff) {
+                    result += p;
+                    break;
+                }
             }
         }
     }
@@ -148,32 +107,12 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(result)
 }
 
+pub fn part_one(input: &str) -> Option<u32> {
+    solve(input, 0)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    let (_, input) = parse_input(input).unwrap();
-
-    let mut result: u32 = 0;
-
-    for pattern in input.patterns.iter() {
-        for p in 1..pattern.rows.len() {
-            let p = p as u32;
-            if is_row_reflection2(&pattern, p) {
-                result += p * 100;
-                break;
-            }
-        }
-
-        let tp = pattern.transpose();
-
-        for p in 1..tp.rows.len() {
-            let p = p as u32;
-            if is_row_reflection2(&tp, p) {
-                result += p;
-                break;
-            }
-        }
-    }
-
-    Some(result)
+    solve(input, 1)
 }
 
 #[cfg(test)]
