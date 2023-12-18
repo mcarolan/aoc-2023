eb60f00b4affd00843ad7419c3be3815fbb61b97
Martin Carolan <martin@typesystems.co.uk>
2023-12-18 16:31:25 +1300

    clean up

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,7 +1,7 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
 use itertools::Itertools;
-use pathfinding::directed::dijkstra::{dijkstra, dijkstra_all};
+use pathfinding::directed::dijkstra::dijkstra;
 
 advent_of_code::solution!(17);
 #[derive(PartialEq, Copy, Clone, Debug, Hash, Eq)]
@@ -66,6 +66,8 @@ fn parse_input(input: &str) -> (HashMap<(i64, i64), i64>, i64, i64) {
 
 fn neighbours(
     grid: &HashMap<(i64, i64), i64>,
+    min_steps: u32,
+    max_steps: u32
 ) -> impl FnMut(
     &((i64, i64), Option<Direction>, u32),
 ) -> Vec<(((i64, i64), Option<Direction>, u32), usize)>
@@ -76,45 +78,12 @@ fn neighbours(
         let steps = *steps;
 
         if let Some(dir) = *dir {
-            if steps < 2 {
-                // we can continue straight
-                let straight = dir.offset(pos);
-                res.push((straight, Some(dir), steps + 1));
-            }
-            let rot = dir.rot();
-            let anti_rot = dir.anti_rot();
-
-            res.push((rot.offset(pos), Some(rot), 0));
-            res.push((anti_rot.offset(pos), Some(anti_rot), 0));
-        } else {
-            res.push((Direction::Right.offset(pos), Some(Direction::Right), 0));
-            res.push((Direction::Down.offset(pos), Some(Direction::Down), 0));
-        }
-
-        res.into_iter()
-            .flat_map(|v| grid.get(&v.0).map(|cost| (v, *cost as usize)))
-            .collect_vec()
-    }
-}
-
-fn neighbours2(
-    grid: &HashMap<(i64, i64), i64>,
-) -> impl FnMut(
-    &((i64, i64), Option<Direction>, u32),
-) -> Vec<(((i64, i64), Option<Direction>, u32), usize)>
-       + '_ {
-    move |(pos, dir, steps)| {
-        let mut res: Vec<((i64, i64), Option<Direction>, u32)> = Vec::new();
-        let pos = *pos;
-        let steps = *steps;
-
-        if let Some(dir) = *dir {
-            if steps < 3 {
+            if (steps as i64) < (min_steps as i64) - 1 {
                 let straight = dir.offset(pos);
                 res.push((straight, Some(dir), steps + 1));
             }
             else {
-                if steps < 9 {
+                if steps < max_steps - 1 {
                     let straight = dir.offset(pos);
                     res.push((straight, Some(dir), steps + 1));
                 }
@@ -141,7 +110,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 
     let start: ((i64, i64), Option<Direction>, u32) = ((0, 0), None, 0);
 
-    dijkstra(&start, neighbours(&input), |((pos), _, _)| {
+    dijkstra(&start, neighbours(&input, 0, 3), |(pos, _, _)| {
         *pos == (rows - 1, cols - 1)
     })
     .map(|(_, c)| c as u32)
@@ -152,7 +121,7 @@ pub fn part_two(input: &str) -> Option<u32> {
 
     let start: ((i64, i64), Option<Direction>, u32) = ((0, 0), None, 0);
 
-    dijkstra(&start, neighbours2(&input), |((pos), _, steps)| {
+    dijkstra(&start, neighbours(&input, 4, 10), |(pos, _, steps)| {
         *pos == (rows - 1, cols - 1) && *steps >= 3
     }).map(|(_, c)| c as u32)
 }
